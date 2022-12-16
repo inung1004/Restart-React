7c5d9c1d8baaba2846edb86a937e55cc1748fd94
안윤지 <dksdbswl10430@naver.com>
2022-12-16 20:19:19 +0900

    🎀 MaterialUI

--- material-ui/src/components/MaterialUI/MaterialUIExample.jsx
+++ material-ui/src/components/MaterialUI/MaterialUIExample.jsx
@@ -1,8 +1,10 @@
+import CenteredGrid from "./Grid";
 import ButtonExample from "./ButtonExample";
 
 export default function MaterialUIExample() {
   return (
     <div>
+      <CenteredGrid />
       <ButtonExample />
     </div>
   );
